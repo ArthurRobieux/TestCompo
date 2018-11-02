d4d7a4b4ca1955fdfa2d2de3ea17c00b2c76e5fd
Arthur <arthur.robieux@gmail.com>
2018-11-02 11:46:32 +0100

    Change player's presence onclick

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import './index.css';
 // App
 
 const members = [
-        {name: 'Arthur', age: '22', buts: '3', pd: '2', carton: '0', presence: '1'},
-        {name: 'Alice', age: '17', buts: '2', pd: '1', carton: '1', presence: '0'},
-        {name: 'Guillaume', age: '48', buts: '0', pd: '4', carton: '0', presence: '1'},
-        {name: 'Claire', age: '47', buts: '1', pd: '3', carton: '0', presence: '0'},
-        {name: 'Thibaud', age: '18', buts: '2', pd: '2', carton: '0', presence: '1'},
-        {name: 'Arnaud', age: '10', buts: '4', pd: '1', carton: '1', presence: '1'},
+        {name: 'Arthur', age: '22', buts: '3', pd: '2', carton: '0', presence: 1},
+        {name: 'Alice', age: '17', buts: '2', pd: '1', carton: '1', presence: 0},
+        {name: 'Guillaume', age: '48', buts: '0', pd: '4', carton: '0', presence: 1},
+        {name: 'Claire', age: '47', buts: '1', pd: '3', carton: '0', presence: 0},
+        {name: 'Thibaud', age: '18', buts: '2', pd: '2', carton: '0', presence: 0},
+        {name: 'Arnaud', age: '10', buts: '4', pd: '1', carton: '1', presence: 1},
     ];
 
 
@@ -34,7 +34,7 @@ class TableApp extends React.Component {
 
           Add Members :<br/><br/>
           Name : <textarea onChange={this.handleChangeName} value={this.state.text_name} />
-          Age : <textarea onChange={this.handleChangeAge} value={this.state.text_age} />
+          Age : <textarea onChange={this.handleChangeAge} value={this.state.text_age} /><br/><br/>
 
           <button>
             Add #{this.state.members.length + 1}
@@ -80,34 +80,64 @@ class TableApp extends React.Component {
 class MemberList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { members_present: [], members_absent: []};
+    this.state = { present_members: [], absent_members: [] };
+    this.change_presence = this.change_presence.bind(this);
+    this.sorting_members = this.sorting_members.bind(this);
+  }
+
+  sorting_members(){
+
+        this.state.present_members = [];
+        this.state.absent_members = [];
+
+        /*this.setState({present_members: [], absent_members: []});*/
+
+      for(var i= 0; i < members.length; i++){
+        if(members[i].presence === 1){
+            this.state.present_members.push(members[i]);
+        }
+        else{
+            this.state.absent_members.push(members[i]);
+        }
+      }
   }
 
 
+  change_presence(item){
+
+      this.setState({});
+
+      for(var i= 0; i < members.length; i++){
+        if(item === members[i]){
+            if(item.presence === 0){members[i].presence=1;}
+            else{members[i].presence=0;}
+        }
+      }
+  }
 
 
   render() {
 
-      const present_members = [];
-      const absent_members = [];
+    //Sorting members
+    this.sorting_members()
 
-      for(var i= 0; i < members.length; i++){
-        if(members[i].presence === "1"){
-            present_members.push(members[i]);
-        }
-        else{
-            absent_members.push(members[i]);
-        }
-      }
 
     return (
       <div class="member_list">
-          <h2>My Table</h2>
-          <h3>Tableau Général</h3>
+
+          <h3>Tableau trié par présence</h3>
 
           <table>
+              <tr class="present_members">
+                  <th>Présents</th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+              </tr>
 
-              <tr>
+              <tr class="header_members">
                   <th>Name</th>
                   <th>Age</th>
                   <th>Buts</th>
@@ -116,29 +146,8 @@ class MemberList extends React.Component {
                   <th>Présence</th>
               </tr>
 
-              {this.props.members.map(item => (
-                  <tr>
-                      <td>{item.name}</td>
-                      <td>{item.age}</td>
-                      <td>{item.buts}</td>
-                      <td>{item.pd}</td>
-                      <td>{item.carton}</td>
-                      <td>{item.presence}</td>
-                  </tr>
-              ))}
-          </table>
-
-          <h3>Tableau trié par présence</h3>
-
-          <table>
-
-
-              <tr class="present_members">
-                  Present
-              </tr>
-
-               {present_members.map(item => (
-                  <tr>
+               {this.state.present_members.map(item => (
+                  <tr class="list_present_members" onClick={() => this.change_presence(item)}>
                       <td>{item.name}</td>
                       <td>{item.age}</td>
                       <td>{item.buts}</td>
@@ -149,11 +158,17 @@ class MemberList extends React.Component {
               ))}
 
               <tr class="absent_members">
-                  Absent
+                  <th>Absents</th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
               </tr>
 
-               {absent_members.map(item => (
-                  <tr>
+               {this.state.absent_members.map(item => (
+                  <tr class="list_absent_members" onClick={() => this.change_presence(item)}>
+
                       <td>{item.name}</td>
                       <td>{item.age}</td>
                       <td>{item.buts}</td>
@@ -165,13 +180,35 @@ class MemberList extends React.Component {
 
           </table>
 
+
+          <h3>Tableau des members</h3>
+
+          <table>
+              <tr className="header_members">
+                  <th>Name</th>
+                  <th>Age</th>
+                  <th>Buts</th>
+                  <th>PD</th>
+                  <th>Cartons</th>
+                  <th>Présence</th>
+              </tr>
+
+              {members.map(item => (
+                  <tr className="list_present_members" onClick={() => this.change_presence(item)}>
+                      <td>{item.name}</td>
+                      <td>{item.age}</td>
+                      <td>{item.buts}</td>
+                      <td>{item.pd}</td>
+                      <td>{item.carton}</td>
+                      <td>{item.presence}</td>
+                  </tr>
+              ))}
+          </table>
+
       </div>
     );
   }
 }
-
-
-
 
 //ReactDOM
 
