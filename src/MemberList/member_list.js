57f661035e041f0de2f883d38664906c468ded62
Arthur <arthur.robieux@gmail.com>
2018-11-07 15:56:03 +0100

    Clean code

--- src/MemberList/member_list.js
+++ src/MemberList/member_list.js
@@ -1,63 +1,68 @@
 import React from "react";
 
 class MemberList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { attendance_group: []};
-  }
+    constructor(props) {
+        super(props);
+        this.state = { attendance_group: [], data: {} }
+    }
 
-  get_attendance_group(){
+    // Get attendance groups in state.data
+    get_attendance_group(){
 
-    this.state.attendance_group = [];
+        this.state.attendance_group = [];
 
-    try {
-        for (var i = 0; i < this.props.data.attendance_groups.length; i++) {
-            this.state.attendance_group.push(this.props.data.attendance_groups[i].slug_name);
+        try {
+            for (var i = 0; i < this.state.data.attendance_groups.length; i++) {
+                this.state.attendance_group.push(this.state.data.attendance_groups[i].slug_name);
+            }
+        }
+        catch(error) {
+            console.error('No data for attendance group');
+        }
+        //console.log(this.state.data);
+    }
+
+    // Show change_presence logo in the table
+    show_change_presence(profile){
+
+        return(
+            <span>
+                {this.state.attendance_group.map(group => (
+                <img className="logo_presence" src={this.get_group_image(group)} alt="yes" onClick={() => this.change_presence(profile.id, group)}/>
+                ))}
+            </span>
+        );
+    }
+
+    // Get the right image for each group
+    get_group_image(group){
+        if(group==='played' || group==='available' || group==='present' || group==='waiting_list' || group==='participant'){
+            return("Yes.png")
+        }
+        else if(group==='rsvp'){
+            return("Waiting.png")
+        }
+        else if(group==='not_selected'){
+            return("Not_Selected.png")
+        }
+        else{
+            return("No.png")
         }
     }
-    catch(error) {
-        console.error('No data for attendance group');
-    }
-    console.log(this.props.data);
-  }
 
-  show_change_presence(profile, current_slug_name){
-
-      return(
-          <span>
-          {this.state.attendance_group.map(group => (
-            <img className="logo_presence" src={this.get_group_image(group)} alt="yes" onClick={() => this.change_presence(profile.id, group)}/>
-          ))}
-          </span>
-      );
-  }
-
-  get_group_image(group){
-      if(group==='played' || group==='available' || group==='present' || group==='waiting_list' || group==='participant'){
-          return("Yes.png")
-      }
-      else if(group==='rsvp'){
-          return("Waiting.png")
-      }
-      else if(group==='not_selected'){
-          return("Not_Selected.png")
-      }
-      else{
-          return("No.png")
-      }
-  }
-
-
-  change_presence(profile_id, attendance){
+    // Call the API in order to change profile attendance
+    change_presence(profile_id, attendance){
 
         this.setState({});
 
-        const event_id = this.props.data.id;
+        const event_id = this.state.data.id;
 
         const API_URL = 'http://api.local.sporteasy.net:8000/v2.1/teams/' + this.props.team_id + '/events/'
-            + event_id + '/profiles/' + profile_id + '/';
-        const bearer = "Bearer b15dfb6dee52b68d5eafe5602ddc79afabf2717a";
+        + event_id + '/profiles/' + profile_id + '/';
+        const bearer = this.props.bearer;
 
+        // Requête pour modifier le groupe
+        // A la fin de la requête on recharge les données de l'API
         fetch(API_URL, {
             method: "PUT",
             headers: {
@@ -67,115 +72,128 @@ class MemberList extends React.Component {
             body: JSON.stringify({
                 "attendance_group": attendance,
             })
-        });
+        })
+        .then(response =>
+        this.get_api_data(),
+        )
+    }
 
+    // Show or hide body of a group
+    show_hide_list(group_slug_name){
+        this.setState({});
 
+        const id = 'body_' + group_slug_name + '_members';
 
-      // ICI ON MET A JOUR LE PROPS.DATA EN FAISANT CHANGE_PRESENCE
+        if(document.getElementById(id).style.display === 'table-row-group') {
+            document.getElementById(id).style.display = 'none';
+        }
+        else{
+            document.getElementById(id).style.display = 'table-row-group';
+        }
+    }
 
-      this.props.data.attendees[1].slug_name = "available";
+    // Functions which define right CSS for groups
 
-  }
+    define_head_id(group){
+        return("header_" + group + "_members");
+    }
 
+    define_head_class(group){
+        return("title_" + group + "_members");
+    }
 
-  show_hide_list(group_slug_name){
-      this.setState({});
+    define_body_id(group){
+        return('body_' + group + "_members");
+    }
 
-      const id = 'body_' + group_slug_name + '_members';
+    define_body_class(group){
+        return(group + "_members");
+    }
 
-      if(document.getElementById(id).style.display === 'table-row-group') {
-          document.getElementById(id).style.display = 'none';
-      }
-      else{
-          document.getElementById(id).style.display = 'table-row-group';
-      }
-  }
+    // Show and create the table
+    show_table(){
+        try {
+            return (
+                // Table
+                <table>
 
-  // 3 Fonctions qui vont charger le bon CSS selon le groupe
+                    {/* On boucle sur chacun des groupes */}
+                    {this.state.data.attendees.map(item => (
+                    <div id="attendance_group">
+                        
+                        <thead id={this.define_head_id(item.slug_name)}>
 
-  define_head_id(group){
-      return("header_" + group + "_members");
-  }
+                            <tr className={this.define_head_class(item.slug_name)} onClick={() => this.show_hide_list(item.slug_name)}>
+                                <th colSpan="5">{item.localized_name} ({item.results.length})</th>
+                            </tr>
 
-  define_head_class(group){
-      return("title_" + group + "_members");
-  }
-
-  define_body_id(group){
-      return('body_' + group + "_members");
-  }
-
-  define_body_class(group){
-      return(group + "_members");
-  }
-
-  // Fonction qui va afficher le tableau avec les données dans props
-
-  show_table(){
-    try {
-        return (
-            // Tableau général
-            <table>
-
-                {/* On boucle sur chacun des groupes */}
-               {this.props.data.attendees.map(item => (
-                   <div id="attendance_group">
-
-
-                      <thead id={this.define_head_id(item.slug_name)}>
-
-                          <tr className={this.define_head_class(item.slug_name)} onClick={() => this.show_hide_list(item.slug_name)}>
-                              <th colSpan="5">{item.localized_name} ({item.results.length})</th>
-                          </tr>
-
-                      </thead>
+                        </thead>
 
                         <tbody id={this.define_body_id(item.slug_name)}>
 
-                           {/* On boucle sur les joueurs du groupe */}
-                           {item.results.map(result => (
+                            {/* On boucle sur les joueurs du groupe */}
+                            {item.results.map(result => (
 
-                              <tr className={this.define_body_class(item.slug_name)} key={result.id} >
-                                  <td>{result.profile.first_name}</td>
-                                  <td>{result.profile.last_name}</td>
-                                  <td>{item.results.length}</td>
-                                  <td>{item.slug_name}</td>
-                                  <td>{this.show_change_presence(result.profile, item.slug_name)}</td>
-                              </tr>
-                               ))}
-                       </tbody>
+                            <tr className={this.define_body_class(item.slug_name)} key={result.id} >
+                                <td>{result.profile.first_name}</td>
+                                <td>{result.profile.last_name}</td>
+                                <td>{item.results.length}</td>
+                                <td>{item.slug_name}</td>
+                                <td>{this.show_change_presence(result.profile, item.slug_name)}</td>
+                            </tr>
+                            ))}
+                        </tbody>
 
 
-                </div>
-              ))}
+                    </div>
+                    ))}
 
-            </table>
+                </table>
 
+            );
+        }
+        catch(error) {
+            console.error('No data for the table');
+            return("Loading..")
+        }
+
+    }
+
+    //Get data from API and stock them in state.data
+    get_api_data(){
+        fetch(this.props.API_URL, {
+            method: "GET",
+            headers: {
+                "Authorization": this.props.bearer,
+                "Content-Type": "application/json",
+            }
+        })
+        .then(response =>
+            response.json()
+        )
+        .then(json_response =>
+            this.setState({ data: json_response}),
+        );
+        console.log(this.state.data);
+    }
+
+    // Call this function at the beginning
+    componentDidMount() {
+        this.get_api_data();
+    }
+
+    // Render Table
+    render() {
+
+        this.get_attendance_group();
+
+        return (
+            <div className="member_list">
+                <h3>Table with API data (id={this.state.data.id})</h3>
+                {this.show_table()}
+            </div>
         );
     }
-      catch(error) {
-        console.error('No data for the table');
-        return("Loading..")
-      }
-
-  }
-
-
-  render() {
-
-    this.get_attendance_group();
-
-    return (
-      <div className="member_list">
-
-          <h3>Tableau automatique (id={this.props.data.id})</h3>
-
-          {this.show_table()}
-
-      </div>
-
-    );
-  }
 }
 
 export default MemberList;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import MemberList from './MemberList/member_list.js';
-import PastMemberList from './MemberList/past_member_list.js';
-import FutureMemberList from './MemberList/future_member_list.js';
-import FutureMemberListWithTreshold from './MemberList/future_member_list_treshold.js';
-
 
 // Constants
 
 const team_id = 6;
-const event_id = 131;    //132 = future, 131 = future with treshold, 74 = past
+const event_id = 132;    //132 = future, 131 = future with treshold, 74 = past
 
 const API_URL = 'http://api.local.sporteasy.net:8000/v2.1/teams/' + team_id +'/events/' + event_id + '/';
 const bearer = "Bearer b15dfb6dee52b68d5eafe5602ddc79afabf2717a";
@@ -22,56 +18,12 @@ const bearer = "Bearer b15dfb6dee52b68d5eafe5602ddc79afabf2717a";
 class TableApp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { data: {}};
   }
-
-    //Show members list Past or Future
-    show_members_list(){
-
-      if(this.state.data.is_past) {
-              return (<PastMemberList data={this.state.data} team_id={team_id}/>);
-      }
-      else{
-          if(this.state.data.available_threshold_reached) {
-              return (<FutureMemberListWithTreshold data={this.state.data} team_id={team_id}/>);
-          }
-          else{
-              return (<FutureMemberList data={this.state.data} team_id={team_id}/>);
-          }
-      }
-    }
-
-    //Get data from API and stock them in TableApp.state.data
-    get_api_data(){
-        fetch(API_URL, {
-            method: "GET",
-            headers: {
-                "Authorization": bearer,
-                "Content-Type": "application/json",
-            }
-        })
-        .then(response =>
-            response.json()
-        )
-        .then(json_response =>
-            this.setState({ data: json_response}),
-        );
-        //console.log(this.state.data);
-        //console.log(this.state.data.id);
-    }
-
-
-    componentDidMount() {
-      // Call this function each second
-      this.get_api_data();
-    }
-
 
   render() {
       return (
           <div>
-              {/*{this.show_members_list()}*/}
-              <MemberList data={this.state.data} team_id={team_id}/>
+              <MemberList team_id={team_id} bearer={bearer} API_URL={API_URL}/>
           </div>
     );
   }
