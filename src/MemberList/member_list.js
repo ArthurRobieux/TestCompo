ed01391e9a2d4f6fea1f11adbbb02fd80c7bb557
Arthur <arthur.robieux@gmail.com>
2018-11-08 14:27:05 +0100

    Add instant table update onclick

--- src/MemberList/member_list.js
+++ src/MemberList/member_list.js
@@ -3,11 +3,12 @@ import React from "react";
 class MemberList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { attendance_group: [], data: {} }
+        this.state = {attendance_group: [], data: {}}
     }
 
     // Get attendance groups in state.data
-    get_attendance_group(){
+    get_attendance_group() {
+
 
         this.state.attendance_group = [];
 
@@ -16,56 +17,53 @@ class MemberList extends React.Component {
                 this.state.attendance_group.push(this.state.data.attendance_groups[i].slug_name);
             }
         }
-        catch(error) {
+        catch (error) {
             console.error('No data for attendance group');
         }
         //console.log(this.state.data);
     }
 
     // Show change_presence logo in the table
-    show_change_presence(profile, group_slug_name){
+    show_change_presence(result_profile, group_slug_name) {
 
-        return(
+        return (
             <span>
-                {this.state.attendance_group.map(logo_group => (
-                <img className="logo_presence" src={this.get_group_image(logo_group)} alt="yes" onClick={() => this.change_presence(profile.id, logo_group, group_slug_name)}/>
+                {this.state.attendance_group.map(logo_group_slug_name => (
+                    <img className="logo_presence" src={this.get_group_image(logo_group_slug_name)} alt="yes"
+                         onClick={() => this.change_presence(result_profile, logo_group_slug_name, group_slug_name)}/>
                 ))}
             </span>
         );
     }
 
     // Get the right image for each group
-    get_group_image(group){
-        if(group==='played' || group==='available' || group==='present' || group==='waiting_list' || group==='participant'){
-            return("Yes.png")
+    get_group_image(group) {
+        if (group === 'played' || group === 'available' || group === 'present' || group === 'waiting_list' || group === 'participant') {
+            return ("Yes.png")
         }
-        else if(group==='rsvp'){
-            return("Waiting.png")
+        else if (group === 'rsvp') {
+            return ("Waiting.png")
         }
-        else if(group==='not_selected'){
-            return("Not_Selected.png")
+        else if (group === 'not_selected') {
+            return ("Not_Selected.png")
         }
-        else{
-            return("No.png")
+        else {
+            return ("No.png")
         }
     }
 
     // Call the API in order to change profile attendance
-    // ID, logo group, row group
-    change_presence(profile_id, attendance_group, group_slug_name){
+    change_presence(result_profile, new_group, previous_group) {
 
         this.setState({});
 
         const event_id = this.state.data.id;
 
         const API_URL = 'http://api.local.sporteasy.net:8000/v2.1/teams/' + this.props.team_id + '/events/'
-        + event_id + '/profiles/' + profile_id + '/';
+            + event_id + '/profiles/' + result_profile.profile.id + '/';
         const bearer = this.props.bearer;
 
-        const id = 'body_' + group_slug_name + "_member_" + profile_id;
-
-        // Requête pour modifier le groupe
-        // A la fin de la requête on recharge les données de l'API
+        // Request to change attendance
         fetch(API_URL, {
             method: "PUT",
             headers: {
@@ -73,106 +71,111 @@ class MemberList extends React.Component {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                "attendance_group": attendance_group,
+                "attendance_group": new_group,
             })
         })
         .then(response =>
-            this.get_api_data(),
-            console.log("update"),
-            document.getElementById("header_played_members").style.opacity = 0.5,
-        )
-        .then(response =>
-            document.getElementById("header_played_members").style.opacity = 1,
-        )
+            console.log("update done")
+        );
+
+        // OnClick, to avoid loading, profile is moved from group list to new group list
+
+        // Get the position of the groups
+        const new_group_pos = this.state.attendance_group.indexOf(new_group);
+        const previous_group_pos = this.state.attendance_group.indexOf(previous_group);
+
+        // Get the profile position in the previous group
+        const profile_pos = this.state.data.attendees[previous_group_pos].results.indexOf(result_profile);
+
+        // Add profile to the new group and delete from the previous
+        this.state.data.attendees[previous_group_pos].results.splice(profile_pos, 1);
+        this.state.data.attendees[new_group_pos].results.push(result_profile);
     }
 
     // Show or hide body of a group
-    show_hide_list(group_slug_name){
+    show_hide_list(group_slug_name) {
         this.setState({});
 
         const id = 'body_' + group_slug_name + '_members';
 
-        if(document.getElementById(id).style.display === 'table-row-group') {
-            document.getElementById(id).style.display = 'none';
-        }
-        else{
+        if (document.getElementById(id).style.display === 'none') {
             document.getElementById(id).style.display = 'table-row-group';
+        }
+        else {
+            document.getElementById(id).style.display = 'none';
         }
     }
 
     // Functions which define right CSS for groups
 
-    define_head_id(group){
-        return("header_" + group + "_members");
+    define_head_id(group) {
+        return ("header_" + group + "_members");
     }
 
-    define_head_class(group){
-        return("title_" + group + "_members");
+    define_head_class(group) {
+        return ("title_" + group + "_members");
     }
 
-    define_body_id(group){
-        return('body_' + group + "_members");
+    define_body_id(group) {
+        return ('body_' + group + "_members");
     }
 
-    define_body_row_id(group, profile_id){
-        return('body_' + group + "_member_" + profile_id);
+    define_body_row_id(group, profile_id) {
+        return (group + "_member_" + profile_id);
     }
 
-    define_body_class(group){
-        return(group + "_members");
+    define_body_class(group) {
+        return (group + "_members");
     }
 
     // Show and create the table
-    show_table(){
+    show_table() {
         try {
             return (
                 // Table
-                <table>
+                <div id="member_list">
 
                     {/* On boucle sur chacun des groupes */}
                     {this.state.data.attendees.map(group => (
-                    <div id={group.slug_name}>
+                        <div id={group.slug_name}>
 
-                        <thead id={this.define_head_id(group.slug_name)}>
+                            <div id={this.define_head_id(group.slug_name)}>
+                                <div className={this.define_head_class(group.slug_name)}
+                                     onClick={() => this.show_hide_list(group.slug_name)}>
+                                    {group.localized_name} ({group.results.length})
+                                </div>
+                            </div>
 
-                            <tr className={this.define_head_class(group.slug_name)} onClick={() => this.show_hide_list(group.slug_name)}>
-                                <th colSpan="5">{group.localized_name} ({group.results.length})</th>
-                            </tr>
+                            <div id={this.define_body_id(group.slug_name)}>
+                                {/* On boucle sur les joueurs du groupe */}
+                                {group.results.map(result_profile => (
+                                    <ul id={this.define_body_row_id(group.slug_name, result_profile.profile.id)}
+                                        className={this.define_body_class(group.slug_name)}>
+                                        <li>{result_profile.profile.first_name}</li>
+                                        <li>{result_profile.profile.last_name}</li>
+                                        <li>{group.results.length}</li>
+                                        <li>{group.slug_name}</li>
+                                        <li>{this.show_change_presence(result_profile, group.slug_name)}</li>
+                                    </ul>
+                                ))}
+                            </div>
 
-                        </thead>
-
-                        <tbody id={this.define_body_id(group.slug_name)}>
-
-                            {/* On boucle sur les joueurs du groupe */}
-                            {group.results.map(result => (
-
-                            <tr id={this.define_body_row_id(group.slug_name, result.profile.id)} className={this.define_body_class(group.slug_name)} key={result.profile.id} >
-                                <td>{result.profile.first_name}</td>
-                                <td>{result.profile.last_name}</td>
-                                <td>{group.results.length}</td>
-                                <td>{group.slug_name}</td>
-                                <td>{this.show_change_presence(result.profile, group.slug_name)}</td>
-                            </tr>
-                            ))}
-                        </tbody>
-
-
-                    </div>
+                        </div>
                     ))}
 
-                </table>
+                </div>
 
             );
         }
-        catch(error) {
+        catch (error) {
             console.error('No data for the table');
-            return("Loading..")
+            return ("Loading..")
         }
 
     }
 
     //Get data from API and stock them in state.data
-    get_api_data(){
+    get_api_data() {
         fetch(this.props.API_URL, {
             method: "GET",
             headers: {
@@ -180,12 +183,12 @@ class MemberList extends React.Component {
                 "Content-Type": "application/json",
             }
         })
-        .then(response =>
-            response.json()
-        )
-        .then(json_response =>
-            this.setState({ data: json_response}),
-        );
+            .then(response =>
+                response.json()
+            )
+            .then(json_response =>
+                this.setState({data: json_response}),
+            );
         console.log(this.state.data);
     }
 
--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ const event_id = 74;    //132 = future, 131 = future with treshold, 74 = past
 const API_URL = 'http://api.local.sporteasy.net:8000/v2.1/teams/' + team_id +'/events/' + event_id + '/';
 const bearer = "Bearer b15dfb6dee52b68d5eafe5602ddc79afabf2717a";
 
-
 // App
 
 class TableApp extends React.Component {
