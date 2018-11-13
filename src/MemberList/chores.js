ef20828b75e89e0130a5a94dfb457585a12d8b11
Arthur <arthur.robieux@gmail.com>
2018-11-13 17:34:02 +0100

    Refactoring : create component Chores

---
+++ src/MemberList/chores.js
@@ -0,0 +1,122 @@
+import React from "react";
+
+class Chores extends React.Component {
+
+
+
+    // Get profiles chores
+    get_profile_chores(profile_id){
+
+        const chores = [];
+
+        // For each chores
+        for(var i=0; i<this.props.member_list.state.data.chores.length; i++){
+
+            // If the player has this chore
+            if(this.props.member_list.state.data.chores[i].profile.id === profile_id){
+
+                // For each profile chore
+                for(var j=0; j<this.props.member_list.state.data.chores[i].chores.length; j++){
+
+                    //console.log(this.props.member_list.state.data.chores[i].chores[j]);
+                    chores.push(this.props.member_list.state.data.chores[i].chores[j]);
+                }
+            }
+        }
+
+        // Show chores in a column
+
+        // No chore
+        if(chores.length === 0){
+            return(
+                <div className={this.props.member_list.state.li_cell_class}>
+                </div>)
+        }
+
+        // One chore
+        else if(chores.length === 1){
+            return(
+                <div className={this.props.member_list.state.li_cell_class}>
+
+                    <img className="icon_task" src={this.create_chore_icon_name(chores[0].icon_name)} alt="icon-task"/>
+
+                    <div className="chore_name">
+                        <img className="icon_delete_chore" src="Images/No.png" alt="delete_chore"
+                             onClick={() => this.unassign_chore(chores[0], profile_id)}/>
+                        {chores[0].name}
+                    </div>
+
+                </div>)
+        }
+
+        // > 1 chores
+        else{
+            return(
+            <div className={this.props.member_list.state.li_cell_class}>
+
+                <img className="icon_task" src="Images/icons_tasks/icon_box.svg" alt="icon-task"/>
+                {chores.length}
+
+                <div className="chore_name">
+
+                    {chores.map(chore => (
+                        this.add_chore_name(chore, profile_id)
+                    ))}
+
+                </div>
+            </div>)
+        }
+
+    }
+
+    create_chore_icon_name(icon_name){
+        return("Images/icons_tasks/" + icon_name + ".svg");
+    }
+
+    add_chore_name(chore, profile_id){
+        return(
+            <span>
+                <img className="icon_delete_chore" src="Images/No.png" alt="delete_chore"
+                     onClick={() => this.unassign_chore(chore, profile_id)}/>
+                {chore.name}
+                <br/>
+            </span>
+        );
+    }
+
+
+    unassign_chore(chore, profile_id){
+
+        this.props.member_list.setState({});
+
+        const API_URL = 'http://api.local.sporteasy.net:8000/v2.1/teams/' + this.props.member_list.props.team_id + '/events/'
+            + this.props.member_list.props.event_id + '/chores/' + profile_id + '/' + chore.id + '/';
+        const bearer = this.props.member_list.props.bearer;
+
+        // Request to change attendance
+        fetch(API_URL, {
+            method: "DELETE",
+            headers: {
+                "Authorization": bearer,
+                "Content-Type": "application/json",
+            },
+        })
+        .then(response =>
+            console.log("Chore " + chore.name + " (" + chore.id + ") deleted of player " + profile_id + "."),
+            this.props.member_list.get_api_data(),
+        );
+    }
+
+
+    // Render Table
+    render() {
+
+        return (
+            <span className={"chores"}>
+                {this.get_profile_chores(this.props.result_profile.profile.id)}
+            </span>
+        );
+    }
+}
+
+export default Chores;
--- src/MemberList/member.js
+++ src/MemberList/member.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Profile from './profile.js';
+import Chores from './chores.js';
 
 class Member extends React.Component {
     constructor(props) {
@@ -93,114 +94,10 @@ class Member extends React.Component {
         return (group + "_members");
     }
 
-
-    // Get profiles chores
-    get_profile_chores(profile_id){
-
-        const chores = [];
-
-        // For each chores
-        for(var i=0; i<this.props.member_list.state.data.chores.length; i++){
-
-            // If the player has this chore
-            if(this.props.member_list.state.data.chores[i].profile.id === profile_id){
-
-                // For each profile chore
-                for(var j=0; j<this.props.member_list.state.data.chores[i].chores.length; j++){
-
-                    //console.log(this.props.member_list.state.data.chores[i].chores[j]);
-                    chores.push(this.props.member_list.state.data.chores[i].chores[j]);
-                }
-            }
-        }
-
-        // Show chores in a column
-
-        // No chore
-        if(chores.length === 0){
-            return(
-                <div id="chores_icons" className={this.props.member_list.state.li_cell_class}>
-                </div>)
-        }
-
-        // One chore
-        else if(chores.length === 1){
-            return(
-                <div id="chores_icons" className={this.props.member_list.state.li_cell_class}>
-
-                    <img className="icon_task" src={this.create_chore_icon_name(chores[0].icon_name)} alt="icon-task"/>
-
-                    <div className="chore_name">
-                        <img className="icon_delete_chore" src="Images/No.png" alt="delete_chore"
-                             onClick={() => this.unassign_chore(chores[0], profile_id)}/>
-                        {chores[0].name}
-                    </div>
-
-                </div>)
-        }
-
-        // > 1 chores
-        else{
-            return(
-            <div id="chores_icons" className={this.props.member_list.state.li_cell_class}>
-
-                <img className="icon_task" src="Images/icons_tasks/icon_box.svg" alt="icon-task"/>
-                {chores.length}
-
-                <div className="chore_name">
-
-                    {chores.map(chore => (
-                        this.add_chore_name(chore, profile_id)
-                    ))}
-
-                </div>
-            </div>)
-        }
-
-    }
-
-    create_chore_icon_name(icon_name){
-        return("Images/icons_tasks/" + icon_name + ".svg");
-    }
-
-    add_chore_name(chore, profile_id){
-        return(
-            <span>
-                <img className="icon_delete_chore" src="Images/No.png" alt="delete_chore"
-                     onClick={() => this.unassign_chore(chore, profile_id)}/>
-                {chore.name}
-                <br/>
-            </span>
-        );
-    }
-
-
-    unassign_chore(chore, profile_id){
-
-        this.props.member_list.setState({});
-
-        const API_URL = 'http://api.local.sporteasy.net:8000/v2.1/teams/' + this.props.member_list.props.team_id + '/events/'
-            + this.props.member_list.props.event_id + '/chores/' + profile_id + '/' + chore.id + '/';
-        const bearer = this.props.member_list.props.bearer;
-
-        // Request to change attendance
-        fetch(API_URL, {
-            method: "DELETE",
-            headers: {
-                "Authorization": bearer,
-                "Content-Type": "application/json",
-            },
-        })
-        .then(response =>
-            console.log("Chore " + chore.name + " (" + chore.id + ") deleted of player " + profile_id + "."),
-            this.props.member_list.get_api_data(),
-        );
-    }
-
     show_past_stats(){
         if(this.props.member_list.state.data.is_past) {
             return (
-                <span>
+                <span className={"stats"}>
                     {/*Stats Buts */}
                     <div className={this.props.member_list.state.li_cell_class}>
                         B
@@ -226,7 +123,7 @@ class Member extends React.Component {
                 {/*Profile*/}
                 <Profile profile={this.props.result_profile.profile}/>
                 {/*Chores*/}
-                {this.get_profile_chores(this.props.result_profile.profile.id)}
+                <Chores member_list={this.props.member_list} result_profile={this.props.result_profile}/>
                 {/*Stats*/}
                 {this.show_past_stats()}
                 {/*Other*/}
